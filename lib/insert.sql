7bb8321dbb815c55c8771927e7127f474a33c621
github username <github email address>
2016-12-08 15:16:33 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (name, age) 
+VALUES ("Tim", 30), ("Mark", 22), ("Marcy", 19), ("Dale", 27), ("Jim", 54), ("Tim", 47), ("John", 34), ("Jen", 18), ("Rod", 31), ("Bob", 40),
+("Rob", 23), ("Tina", 33), ("Lisa", 24), ("Joe", 38), ("Mike", 21), ("Amber", 38), ("Adam", 26), ("Chris", 39), ("Eric", 33), ("Kyle", 45);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("A Plan", "technology", 10000, 2008, 2009), 
+("B Plan", "fishing", 7000, 2002, 2010), 
+("C Plan", "shopping", 20000, 1999, 2016), 
+("D Plan", "business", 50000, 1980, 2015), 
+("E PLan", "government", 20000000000, 1970, 2010), 
+("F Plan", "conservation", 50000, 1982, 2004), 
+("G Plan", "entertainment", 1000000, 1987, 2007), 
+("H Plan", "comedy", 70000, 1972, 2016), 
+("I Plan", "strategy", 100000, 2003, 2011), 
+("J Plan", "sports", 400000, 2001, 2012);
+
+INSERT INTO pledges (amount)
+VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12), (13), (14), (15),
+ (16), (17), (18), (19), (20), (21), (22), (23), (24), (25), (26), (27), (28), (29), (30);
+
